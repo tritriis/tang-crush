e6795f8625598344154988b64aef9920c64a3190
tritriis <85286804+tritriis@users.noreply.github.com>
2021-06-03 17:21:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG  = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường.
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim anh lẻ loi,
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
